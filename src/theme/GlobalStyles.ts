2cfa69511de30b6fe3e7a70aefe9f739e3d366ad
umesh kadam <umeshk149@gmail.com>
2021-11-03 11:19:58 +0530

    added styled components with theme

---
+++ src/theme/GlobalStyles.ts
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components";
+
+// can be used for global styling.
+// We have used modern-normalize.css Cdn in .html
+
+export const GlobalStyles = createGlobalStyle`
+  body {
+      /* css */
+  }
+`;
---
+++ src/theme/theme.ts
@@ -0,0 +1,8 @@
+import { DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
+  colors: {
+    main: "red",
+    secondary: "green",
+  },
+};
